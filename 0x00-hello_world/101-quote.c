e4db74700dd67b5aed550d587f71199758f1afa4
Firdaous02 <elhaskourifirdaous@gmail.com>
2023-06-15 18:06:15 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,17 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE -o `echo $CFILE | cut -d. -f1`.s
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: printing with write function
+ *
+ * Return: 1 (not success)
+*/
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
+}
