6a334ac2af194618b14db93c2ea447963427bfe8
Firdaous02 <elhaskourifirdaous@gmail.com>
2023-06-30 14:29:19 -0700

    4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - function
+ *
+ * @a: first input
+ * @n: second input
+ *
+ * Return: result
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i, j, t;
+
+	for (i  = 0, j = (n - 1); i < j; i++, j++)
+	{
+		t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+}
