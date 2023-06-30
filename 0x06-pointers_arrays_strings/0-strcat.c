39dbced3d0409b2dd81eaf5ea4140a03bb198bc3
Firdaous02 <elhaskourifirdaous@gmail.com>
2023-06-30 12:16:22 -0700

    1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - function
+ *
+ * @dest: first input
+ * @src: second input
+ *
+ * Return: result
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int c, c2;
+
+	c = 0;
+
+	while (dest[c])
+		c++;
+
+	for (c2 = 0; src[c2]; c2++)
+		dest[c++] = src[c2];
+
+	return (dest);
+}
