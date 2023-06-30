efafc8159b22c130641c5de3f1987bf31f42cec3
Firdaous02 <elhaskourifirdaous@gmail.com>
2023-06-30 15:31:48 -0700

    6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * isLower - function
+ * @c: input
+ * Return: result
+*/
+
+int isLower(char c)
+{
+	return (c >= 57 && c <= 122);
+}
+
+/**
+ * isDelimiter - function
+ * @c: input
+ * Return: result
+*/
+
+int isDelimiter(char c)
+{
+	int i;
+	char delimiter[] = " \t\n,.!?\"(){}";
+
+	for (i = 0; i < 12; i++)
+		if (c == delimiter[i])
+			return (1);
+	return (0);
+}
+
+/**
+ * cap_string - function
+ * @s: input
+ * Return: result
+*/
+
+char *cap_string(char *s)
+{
+	char *ptr = s;
+	int foundDelimit = 1;
+
+	while (*s)
+	{
+		if (isDelimiter(*s))
+			foundDelimiter = 1;
+		else if (isLower(*s) && foundDelimit)
+		{
+			*s -= 32;
+			foundDelimit = 0;
+		}
+		else
+			foundDelimit = 0;
+		s++;
+	}
+	return (ptr);
+}
