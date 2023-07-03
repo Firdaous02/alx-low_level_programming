b2df322b6993226b1f212c36d43d262f99333cf5
Firdaous02 <elhaskourifirdaous@gmail.com>
2023-07-03 07:23:51 -0700

    4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/***
+ * _strpbrk - function
+ * @s: first input
+ * @accept: second input
+ * Return: result
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	char *p;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == s[i])
+			{
+				p = &s[i];
+				return (p);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
