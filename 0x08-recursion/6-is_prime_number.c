9dbb41d17d80503a12b099e1c1c4ef39c8b56bcb
Firdaous02 <elhaskourifirdaous@gmail.com>
2023-07-05 07:28:46 -0700

    6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+int prime(int n, int i);
+
+/**
+ * is_prime_number - function
+ * @n: input
+ * Return: result
+*/
+
+int is_prime_number(int n)
+{
+	return (actual_prime(n, 2));
+}
+
+/**
+ * prime - function
+ * @n: first input
+ * @i: second input
+ * Return: result
+*/
+
+int prime(int n, int i)
+{
+	if (i >= n && n > 1)
+		return (1);
+	if (n % i == 0 || n <= 1)
+		return (0);
+	return (prime(n, i + 1));
+}
