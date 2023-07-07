7b534585bad090f82e716e617487710041eda4ff
Firdaous02 <elhaskourifirdaous@gmail.com>
2023-07-07 10:28:21 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: number of arguments
+ * @argv: arguments values
+ * Return: 0 success
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
