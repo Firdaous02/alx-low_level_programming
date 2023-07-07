692a4ea60a422301b90f391e58f80c87e1c73160
Firdaous02 <elhaskourifirdaous@gmail.com>
2023-07-07 11:04:38 -0700

    2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: arguments values
+ * Return: 0 success
+*/
+
+int main(int argc, char const *argv[])
+{
+	int i = 0;
+
+	while (arg--)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+
+	return (0);
+}
