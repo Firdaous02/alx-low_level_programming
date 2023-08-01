e64679d93897d3121f4acf7b8b40cec57c69ad1c
Firdaous02 <elhaskourifirdaous@gmail.com>
2023-08-01 06:52:08 -0700

    123

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds a new node at the beginning of a linked list
+ * @head: pointer to the first node in the list
+ * @n: data to insert in that new node
+ *
+ * Return: pointer to the new node, or NULL if it fails
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *nv =(listint_t *)malloc(sizeof(listint_t));
+	if (!nv)
+		return (NULL);
+
+	nv->n = n;
+	nv->next = *head;
+	*head = nv;
+
+	return (nv);
+}
---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - Function that adds a node at the end of a linked list
+ * @head: pointer to the first element in the list
+ * @n: data to insert in the new element
+ *
+ * Return: pointer to the new node, or NULL if it fails
+*/
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *nv = (listint_t *)malloc(sizeof(listint_t));
+	listint_t *tmp = *head;
+
+	nv->n = n;
+	nv->next = NULL;
+
+	if (*head == NULL)
+		*head = nv;
+	else
+	{
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+
+		tmp->next = nv;
+	return (nv);
+}
